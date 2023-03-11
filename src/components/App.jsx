7fdaf77655aa66a0b925efd2dab1b3b6b2c74dec
Dariaaal <1406dariaal@gmail.com>
2023-03-12 01:14:15 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import React, { Component } from "react";
+import Statistics from "./feedback/Statistics";
+import Feedback from "./feedback/Feedback";
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Feedback/>
+      <Statistics/>
     </div>
   );
 };
---
+++ src/components/feedback/Statistics.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Statistics extends Component {
+    render(){
+        return (
+             <div>
+                <h2>Statistics</h2>
+                <p>Good: </p>
+                <p>Neutral: </p>
+                <p>Bad: </p>
+             </div>
+        )
+    }
+}
+
+export default Statistics;
